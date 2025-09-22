f4d975f5e2218078c919d9e8605a9c1793d7eea2
dezmediah <41761092+dezmediah@users.noreply.github.com>
2025-09-22 18:40:53 +0000

    Deploying to gh-pages from @ dezmediah/dezmediah.github.io@1d48f60e10f5e8a9fd9fb10019fc9faf254846f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-generative-poetry",
-          title: 'Generative Poetry',
-          description: "",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
