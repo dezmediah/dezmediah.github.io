2476239fd939de2a64fd786cc297beda2f37e3a0
dezmediah <41761092+dezmediah@users.noreply.github.com>
2025-09-26 21:39:28 +0000

    Deploying to gh-pages from @ dezmediah/dezmediah.github.io@777a4bab72f0e4101181b991a11955fc5705f584 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,9 +429,9 @@ ninja.data = [{
           description: "music videos, an album, and one E.P.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-that-strange-sensation",
+          title: 'That Strange Sensation',
+          description: "a serialized sci-fi story",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
